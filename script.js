b7ca59fb8c28a78ff78fe13feb97e9ad1186977a
Uzoma-Salome <uzomasalome2003@gmail.com>
2025-03-26 14:45:29 +0100

    Task

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ const navList = document.querySelector(".nav-list");
 menuToggle.addEventListener("click", () => {
     navList.classList.toggle("active");
 });
+function nextImage(button) {
+    let product = button.closest(".product");
+    let images = product.querySelectorAll(".product-image img");
+    let activeIndex = Array.from(images).findIndex(img => img.classList.contains("active"));
+
+    images[activeIndex].classList.remove("active");
+    let nextIndex = (activeIndex + 1) % images.length;
+    images[nextIndex].classList.add("active");
+}
+function prevImage(button) {
+    let product = button.closest(".product");
+    let images = product.querySelectorAll(".product-image img");
+    let activeIndex = Array.from(images).findIndex(img => img.classList.contains("active"));
+
+    images[activeIndex].classList.remove("active");
+    let prevIndex = (activeIndex - 1 + images.length) % images.length;
+    images[prevIndex].classList.add("active");
+}
